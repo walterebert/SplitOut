3ba83be6c51ded97c0f2fd33a7e1b23fe3b3a026
Arne Blankerts <Arne@Blankerts.de>
2011-02-21 14:06:12 +0000

    Refactor session, basic Event

--- src/model/Event.php
+++ src/model/Event.php
@@ -5,6 +5,18 @@ namespace SplitOut\Model;
 class Event {
 
    protected $sessions = array();
+   protected $title;
+    
+   public function __construct($title) {
+      if (!is_string($title) || $title=='') {
+         throw new EventException('Title must be a string and not empty');
+      }
+      $this->title = $title;
+   }
+
+   public function getTitle() {
+      return $this->title;
+   }
 
    public function addSession(Session $session) {
       $this->sessions[] = $session;
@@ -12,3 +24,4 @@ class Event {
    
 }
 
+class EventException extends \Exception {}
