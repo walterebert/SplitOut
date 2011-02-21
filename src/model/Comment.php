c55058fa58d0531b4b0c8a5eab3a153a4f738a3d
Stefan Priebsch <stefan@priebsch.de>
2011-02-21 15:13:49 +0100

    Add some meaning to Comment.

--- src/model/Comment.php
+++ src/model/Comment.php
@@ -3,5 +3,30 @@
 namespace SplitOut\Model;
 
 class Comment {
-}
 
+    protected $author;
+    protected $session;
+    protected $text;
+
+    public function __construct(User $author, Session $session, $text)
+    {
+        $this->author = $author;
+        $this->session = $session;
+        $this->text = $text;
+    }
+    
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+    
+    public function getSession()
+    {
+        return $this->session;
+    }
+    
+    public function getText()
+    {
+        return $this->text;
+    }
+}
